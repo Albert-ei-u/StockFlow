182fdea5a112410feda013ad51683f06979451a9
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-15 23:21:40 +0200

    committed

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -188,6 +188,7 @@ const Register = ({ onLogin }) => {
                   ) : (
                     <Eye className="h-5 w-5 text-gray-400 hover:text-gray-600" />
                   )}
+                  
                 </button>
               </div>
             </div>

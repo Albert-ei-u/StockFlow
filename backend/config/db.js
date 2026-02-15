2d4a800dc0bb8cfd6fb0080c30836170be3f62eb
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-15 23:24:06 +0200

    changes made in the files

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,8 @@ export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
+    } 
+    catch (error) {
         console.error(`MongoDB connection error: ${error.message}`);
         process.exit(1);    
     }

d109502ec3fcb180f4900f4a35c14c19240d31b5
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-17 21:56:47 +0200

    configured

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ import Business from '../models/Business.js';
 
 // Generate JWT Token
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || 'sales flow isunderdevelopment', {
+  return jwt.sign({ id }, process.env.JWT_SECRET || 'sales flow is underdevelopment', {
     expiresIn: '30d'
   });
 };

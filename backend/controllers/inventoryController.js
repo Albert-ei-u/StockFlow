dae18cd8ea9ea979c0cd6b8d2502175e4ec8f567
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-18 23:02:48 +0200

    staged Up

--- backend/controllers/inventoryController.js
+++ backend/controllers/inventoryController.js
@@ -2,7 +2,7 @@ import Inventory from '../models/Inventory.js';
 import Product from '../models/Product.js';
 
 // @desc  Get all inventory
-// @route  GET /api/inventory
+// @route GET /api/inventory
 export const getInventory = async (req, res) => {
     try {
         const inventory = await Inventory.find()

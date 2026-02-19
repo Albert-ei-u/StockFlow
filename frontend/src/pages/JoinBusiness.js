8030738eaff302484dd7f093dc808ea4e4d1f954
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-02-19 22:00:53 +0200

    updates made

--- frontend/src/pages/JoinBusiness.js
+++ frontend/src/pages/JoinBusiness.js
@@ -95,6 +95,7 @@ const JoinBusiness = ({ onLogin }) => {
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                     <User className="h-5 w-5 text-gray-400" />
                   </div>
+                  
                   <input
                     id="name"
                     name="name"

46664eb1c528edf98d42e38ff2c37202e2faa388
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-05-09 20:04:05 +0200

    services

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -29,5 +29,4 @@ router.post('/request-password-reset', requestPasswordReset);
 router.post('/verify-reset-code-and-reset-password', verifyResetCodeAndResetPassword);
 // Get Profile (protected)
 router.get('/profile', protect, getProfile);
-
 export default router;

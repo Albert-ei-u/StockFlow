020d0468d940feaf2f60fb58826c4784a45c983f
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-05-11 18:37:11 +0200

    sanitized the data

--- backend/middleware/security.js
+++ backend/middleware/security.js
@@ -6,7 +6,6 @@ import hpp from 'hpp';
 // Custom MongoDB injection protection
 export const sanitizeData = (req, res, next) => {
   const sanitize = (obj) => {
-    if (typeof obj !== 'object' || obj === null) {
       return obj;
     }
     

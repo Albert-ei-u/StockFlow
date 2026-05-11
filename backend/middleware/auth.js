cf4062403768f916e9394d9b39bc7cc09aa2c7a8
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-05-11 18:35:52 +0200

    tx configured

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,7 +10,6 @@ export const protect = async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
       token = req.headers.authorization.split(' ')[1];
     }
-
     // Check if token exists
     if (!token) {
       return res.status(401).json({ 

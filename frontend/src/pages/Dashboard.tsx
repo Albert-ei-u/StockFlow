1b6aefafed78846d90a86ff7217762fceb0eae6e
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-05-15 20:04:05 +0200

    services

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -9,7 +9,6 @@ const Dashboard: React.FC = () => {
   const [lowStockItems, setLowStockItems] = useState<Inventory[]>([]);
   const [loading, setLoading] = useState(true);
 
-  
   useEffect(() => {
     fetchData();
   }, []);
